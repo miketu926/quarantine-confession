2593e7640a9f6ad8ffeb962f3c5d7aa73fc3ae78
Mike <michael.obsessvr.com>
2020-04-01 00:24:18 -0400

    Updated App.js to functional component and added file structure

--- App.js
+++ App.js
@@ -6,10 +6,10 @@
  * @flow
  */
 
-import React, { Component } from "react";
-import { Platform, StyleSheet, Text, View } from "react-native";
+import React, { Component } from 'react';
+import { Platform, StyleSheet, Text, View } from 'react-native';
 
-import firebase from "@react-native-firebase/app";
+import firebase from '@react-native-firebase/app';
 
 // TODO(you): import any additional firebase services that you require for your app, e.g for auth:
 //    1) install the npm package: `yarn add @react-native-firebase/auth@alpha` - you do not need to
@@ -19,50 +19,48 @@ import firebase from "@react-native-firebase/app";
 //    4) The Firebase Auth service is now available to use here: `firebase.auth().currentUser`
 
 const instructions = Platform.select({
-  ios: "Press Cmd+R to reload,\nCmd+D or shake for dev menu",
+  ios: 'Press Cmd+R to reload,\nCmd+D or shake for dev menu',
   android:
-    "Double tap R on your keyboard to reload,\nShake or press menu button for dev menu"
+    'Double tap R on your keyboard to reload,\nShake or press menu button for dev menu'
 });
 
 const firebaseCredentials = Platform.select({
-  ios: "https://invertase.link/firebase-ios",
-  android: "https://invertase.link/firebase-android"
+  ios: 'https://invertase.link/firebase-ios',
+  android: 'https://invertase.link/firebase-android'
 });
 
-type Props = {};
+const App = () => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.welcome}>Welcome to React Native + Firebase!</Text>
+      <Text style={styles.instructions}>To get started, edit App.js</Text>
+      <Text style={styles.instructions}>{instructions}</Text>
+      {!firebase.apps.length && (
+        <Text style={styles.instructions}>
+          {`\nYou currently have no Firebase apps registered, this most likely means you've not downloaded your project credentials. Visit the link below to learn more. \n\n ${firebaseCredentials}`}
+        </Text>
+      )}
+    </View>
+  );
+};
 
-export default class App extends Component<Props> {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native + Firebase!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
-        {!firebase.apps.length && (
-          <Text style={styles.instructions}>
-            {`\nYou currently have no Firebase apps registered, this most likely means you've not downloaded your project credentials. Visit the link below to learn more. \n\n ${firebaseCredentials}`}
-          </Text>
-        )}
-      </View>
-    );
-  }
-}
+export default App;
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    backgroundColor: "#F5FCFF"
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF'
   },
   welcome: {
     fontSize: 20,
-    textAlign: "center",
-    margin: 10
+    textAlign: 'center',
+    margin: 10,
   },
   instructions: {
-    textAlign: "center",
-    color: "#333333",
-    marginBottom: 5
-  }
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
 });
